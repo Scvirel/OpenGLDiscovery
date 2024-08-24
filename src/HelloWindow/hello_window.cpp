188c606e03d623918723b802369f2bed66e685eb
Scvirel <ScvirelVlad@gmail.com>
2024-08-25 00:28:12 +0300

    Hello window!

--- src/HelloWindow/hello_window.cpp
+++ src/HelloWindow/hello_window.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main()
 {
@@ -44,6 +45,12 @@ int main()
 	//Render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+
+		//Rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -55,4 +62,12 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
 }
